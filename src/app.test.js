2bb195d3988965d3f011c06397c3826068c15681
PanisaBD <panisa2548@gmail.com>
2025-04-24 10:43:08 +0700

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,7 @@
 const { dayOfTheWeek } = require('./app');
-const { monthsOfYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
 
-test('getDay returns the month of year', () => {
-    const month = monthsOfYear( new Date('3/11/2020') );
-    expect( month ).toBe('November');
-});
